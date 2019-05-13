ef89a7208d5257f756a34c2c847e216a98216c0d
torianne02 <victoria.fluharty@gmail.com>
2019-05-13 16:14:44 -0700

    changes ToppingSearch.js from a dropdown to a form

--- src/components/pizzas/ToppingSearch.js
+++ src/components/pizzas/ToppingSearch.js
@@ -1,45 +1,20 @@
 import React, { Component } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class ToppingSearch extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false,
-      toppings: [],
-    };
-  }
-
-  toggle() {
-    this.setState(prevState => ({
-      dropdownOpen: !prevState.dropdownOpen
-    }));
-  }
-
-  getDropdownItems() {
-    return (
-      this.state.toppings.map((topping) => {
-        return (<DropdownItem>{ topping }</DropdownItem>)
-      })
-    )
-  }
-
-  render() {
-    return (
-      <div className="dropdown-menu">
-        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          <DropdownToggle caret>
-            Pizza by Topping
-          </DropdownToggle>
-          <DropdownMenu>
-            { this.getDropdownItems() }
-          </DropdownMenu>
-        </Dropdown>
-      </div>
-    )
-  }
+const ToppingSearch = props => {
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="topping-search">Search Pizzas by Topping</Label>
+        <Input type="select" name="select">
+          { this.props.toppings.map((topping) => {
+            return <option>topping</option>
+          }) }
+        </Input>
+        <Button>Search</Button>
+      </FormGroup>
+    </Form>
+  )
 }
 
 export default ToppingSearch
