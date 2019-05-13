5b3710937b01469bd528bbdb32f12ae8d3b973c2
torianne02 <victoria.fluharty@gmail.com>
2019-05-13 13:04:40 -0700

    imports Person component in PersonContainer git status ^ ignore line above renders Person component

--- src/containers/PeopleContainer.js
+++ src/containers/PeopleContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchForm from '../components/people/SearchForm';
+import Person from '../components/people/Person';
 
 class PeopleContainer extends Component {
   constructor(props) {
@@ -43,24 +44,29 @@ class PeopleContainer extends Component {
       })
       .then((response) => response.json())
       .then((data) => console.log(data))
-    // const api_call = await fetch(`http://127.0.0.1:9393/api/v1/people?name=${name}`)
-    // const response = await api_call.json();
-    // console.log(response)
     // this.setState({
     //   name: response.name,
-    //   // pizzas: response.pizzas NEED TO FIGURE THIS OUT
+    //   pizzas: response[1]
     //   submitted: false,
     // })
   }
 
   render() {
     return (
-      <div className="name-search">
-        <SearchForm
-          nameValue={this.state.name}
-          handleOnChange={this.handleOnChange}
-          handleOnSubmit={this.handleOnSubmit}
-        />
+      <div className="people">
+        <div className="name-search">
+          <SearchForm
+            nameValue={this.state.name}
+            handleOnChange={this.handleOnChange}
+            handleOnSubmit={this.handleOnSubmit}
+          />
+        </div>
+        <div className="person-info">
+          <Person
+            name={this.state.name}
+            pizzas={this.state.pizzas}
+          />
+        </div>
       </div>
     )
   }
