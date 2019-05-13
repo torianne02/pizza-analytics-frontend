64e6e88f4b206ef528ed394514ed4a110c6a6825
torianne02 <victoria.fluharty@gmail.com>
2019-05-13 13:27:01 -0700

    imports CardText adds () around return statement in MonthlyHigh comp

--- src/components/pizzas/MonthlyHigh.js
+++ src/components/pizzas/MonthlyHigh.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Card, CardTitle, CardSubtitle, CardBody} from 'reactstrap';
+import { Row, Card, CardTitle, CardSubtitle, CardBody, CardText } from 'reactstrap';
 
 const MonthlyHigh = ( props ) =>
   <div className="monthly-high">
@@ -8,7 +8,7 @@ const MonthlyHigh = ( props ) =>
         <CardTitle>Highest Day of Consumption - Monthly</CardTitle>
         <CardText>
           { props.monthlyHigh.map((month) => {
-            return <Row>{ month.date_consumed } | { month.num_pizzas }</Row>
+            return (<Row>{ month.date_consumed } | { month.num_pizzas }</Row>)
           })}
         </CardText>
       </CardBody>
