13e689f1a79505bb27f64bd889eca35d693c47b8
torianne02 <victoria.fluharty@gmail.com>
2019-05-13 20:32:22 -0700

    fixes error in MonthlyHigh wrote id= instead of key=

--- src/components/pizzas/MonthlyHigh.js
+++ src/components/pizzas/MonthlyHigh.js
@@ -8,7 +8,7 @@ const MonthlyHigh = ( props ) =>
         <CardTitle>Highest Day of Consumption - Monthly</CardTitle>
         <CardText>
           { props.monthlySales.map((month, i) => {
-            return (<Row id={ i }>{ month.date_consumed } | { month.num_sales }</Row>)
+            return <Row key={ i }>{ month.date_consumed } | { month.num_sales }</Row>
           })}
         </CardText>
       </CardBody>
