b74b9802a169ea2442a506ffbd6e0289d9e0ff10
torianne02 <victoria.fluharty@gmail.com>
2019-05-13 17:31:33 -0700

    creates routes and imports multiple containers to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PizzasContainer from './containers/PizzasContainer';
 import PeopleContainer from './containers/PeopleContainer';
+import NameSearchContainer from './containers/NameSearchContainer';
+import ToppingSearchContainer from './containers/ToppingSearchContainer';
 import './App.css';
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path='/' component={PizzasContainer} />
+          <Route exact path='/topping-search' component={ToppingSearchContainer} />
           <Route exact path='/people' component={PeopleContainer} />
+          <Route exact path='/name-search' component={NameSearchContainer} />
         </Switch>
       </div>
     </Router>
